6e99bb3caffb635f92253591f92cb6cc7dabc58c
Mareedu Venkata Pavan <84392859+venkatapavan99@users.noreply.github.com>
2025-12-19 07:00:43 +0530

    Fix syntax error in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         int x=10;
-        System.out.println("Hello Jenkins + SonarCloud")
+        System.out.println("Hello Jenkins + SonarCloud");
     }
 }
